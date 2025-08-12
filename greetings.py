abae7bd7e0b895d2b61d2bac9d46df630e883cbf
Vivian05-joho <genevieve2852@gmail.com>
2025-08-12 21:14:52 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,65 @@
+print("Hello There")
+
+
+
+site_name = "Power Learn Project"
+
+print(site_name)
+
+site_name = "I love you"
+
+site_name_1, site_name_2 = 5,7
+site_name_1
+
+#' Python is case_sensitive. Hence there is a difference between Num and num
+
+num = 32
+type (num)
+
+languages = ["Python","Web"]
+
+print(languages[0])
+
+
+# lets start to build a data frame / mrore like a table
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+print(capital_city)
+
+#Python operators
+# ==, !=, >, <, <=,>=
+
+excel = 5>3
+print
+
+# Use "Input" if you want a reactive portal. See below run
+example_name = input ("What is your name?")
+
+type (example_name)
+
+
+# we need to create a way through which the user can input 2 variables
+# then the app should do a mathematical computaiton and give out an output.
+
+# float() is used so the inputs can be decimals.
+# Get input from the user
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+result = a / b
+print (result )
+# Perform division
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
